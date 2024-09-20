c4f8e8e1d5ebc6d0e3729a63c20f0530f4fc1c7f
PraveenSeeme <seemepraveen77@gmail.com>
2024-09-20 06:53:21 +0000

    create deployment script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Updating System"
+sudo apt -y update
+
+echo "Installing Utilities"
+sudo apt -y install zip unzip
+
+echo "Installing NGINX Web Server"
+sudo apt -y install nginx
+
+echo "cleanup Nginx Document Root"
+sudo rm -rf /var/www/html/*
+
+echo "clonning Login App To NGINX"
+sudo git clone https://github.com/PraveenSeeme/Login-2424.git /var/www/html
+
+echo "Script Execution Completed"
